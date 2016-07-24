10bb66ffcf263f0d9a55bb17b629d424f3f8f01e
Choi2 <csj21300@naver.com>
2016-07-25 08:22:03 +0900

    Add files via upload
    1. 초음파센서 2개 사용
    2. 위치는 오른쪽 모서리에 위치
    3. 사람이 앉으면 불이 켜지고, 나가면 불이 꺼짐. 
    4. 주석은 영어로...

---
+++ Demo_Seat_Management_System/160725_Demo_Seat_Management_System_SeongJin.ino.ino
@@ -0,0 +1,109 @@
+
+#define trigPin1 12
+#define trigPin2 8
+
+#define echoPin1 13
+#define echoPin2 9
+
+#define buttonPin 2
+#define ledPin 3
+#define error 10
+#define limit 1000
+#define trigoffdelay 20000
+#define trigondelay 10000
+
+#define count 2
+
+int ledState = LOW;
+int buttonState;
+int lastButtonState = LOW;
+long lastDebounceTime = 0;
+long debounceDelay = 50;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, ledState);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(trigPin2, OUTPUT);
+
+  pinMode(echoPin1, INPUT);
+  pinMode(echoPin2, INPUT);
+
+}
+
+void loop()
+{
+  int person;
+  long start = 0, check[count] = {0};
+  long duration[count] = {0}, distance[count] = {0}, sum[count] = {0}, avr[count] = {0};
+
+
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(trigPin1, LOW);
+    delayMicroseconds(trigoffdelay);
+    digitalWrite(trigPin1, HIGH);
+    delayMicroseconds(trigondelay);
+    digitalWrite(trigPin1, LOW);
+    duration[0] = pulseIn(echoPin1, HIGH);
+    digitalWrite(trigPin2, LOW);
+    delayMicroseconds(trigoffdelay);
+    digitalWrite(trigPin2, HIGH);
+    delayMicroseconds(trigondelay);
+    digitalWrite(trigPin2, LOW);
+    duration[1] = pulseIn(echoPin2, HIGH);
+
+
+    for (int a = 0; a < count; a++)
+      distance[a] = duration[a] * 17 / 1000;
+
+
+
+    delay(1);
+
+    for (int a = 0; a < count; a++) {
+      if (distance[a] >= limit) {
+        i = -1;
+        for (int b = 0; b < count; b++) {
+          sum[b] = 0;
+        }
+        continue;
+      }
+    }
+
+    delay(1);
+
+    for (int a = 0; a < count; a++)
+      sum[a] = sum[a] + distance[a];
+
+    //delay(1000);
+  }
+  //Serial.println(sum[0]);
+
+  for (int a = 0; a < count; a++)
+    avr[a] = (sum[a] / 10);
+
+    Serial.print("avr[0] = ");
+    Serial.println(avr[0]);
+    Serial.print("avr[1] = ");
+    Serial.println(avr[1]);
+
+    if((avr[0] < 140) && (avr[1] < 140)) //사람이 있다면?
+    {
+      //  Serial.println("111111111111111");
+         digitalWrite(ledPin, HIGH);
+         
+    }
+    else //없다면? 
+    {
+     //  Serial.println("00000000000000");
+        digitalWrite(ledPin, LOW);
+    }
+}
+
+
+
+
+
